f7664e4bfc164824039e8bacb88f467fb97547fb
mmkhan <mariatechfuture@gmail.com>
2020-04-20 20:32:14 -0400

    created files and laid out foundation

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector("#question-container");
+document.querySelector("#minutes");
+document.querySelector("#hours");
+document.querySelector("#seconds");
+document.querySelector("#start-btn");
+
+var score;
+score = 0;
+
